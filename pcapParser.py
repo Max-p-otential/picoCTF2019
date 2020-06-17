842815154b7a8ad89115787977b217325093ba72
Alessandro <alessandroescher@gmx.net>
2020-06-17 17:06:11 +0200

    HexEdited mystery.png
    mystery image with fixed header, running pngcheck returns OK but the image still doesn't display

--- pcapParser.py
+++ pcapParser.py
@@ -8,16 +8,22 @@ def main():
 	if(len(arguments)==0):
 		f = open("capture.pcap", "rb")
 	if(len(arguments)>=1):
-		f = open(arguments[argc-1], "rb")
+		f = open(arguments[1], "rb")
+  
+	# open a dpkt reader to our pcap file
 	pcap = dpkt.pcap.Reader(f)
 	data = ""
 
+	# iterate through all entries (ts = timestamp, buf = packet)
 	for ts, buf in pcap:
+		# create an Ethernet instance and check if our packet is an IP packet
 		eth = dpkt.ethernet.Ethernet(buf)
 		if isinstance(eth.data, dpkt.ip.IP):
 			ip = eth.data
+			# check for UDP
 			if isinstance(ip.data, dpkt.udp.UDP):
 				udp = ip.data
+				# filter for destination port
 				if udp.dport == 8888:
 					data += udp.data.decode("utf-8")
 	
