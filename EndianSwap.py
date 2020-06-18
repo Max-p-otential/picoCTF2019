f7b2b1004f090abbcf65b2aa99e7a26e82134b21
Maximilian P <maximilian.pursche@web.de>
2020-06-18 17:34:48 +0200

    EndianSwap works but is slow as fuck

---
+++ EndianSwap.py
@@ -0,0 +1,50 @@
+import sys
+
+class EndSwap:
+    def __init__ (self,filepath,size):
+        try:
+            self.byteList=list(open(filepath,"rb").read())
+        except FileNotFoundError:
+            print ("File A doesn't exist! Maybe it is spelled wrong?")
+            exit()
+        except:
+            print ("Something went wrong!")
+            exit()
+        self.result=self.Swapbytes(self.byteList,size)
+    
+    def Swapbytes (self,bytelist,wsize):
+        result=[]
+        lenn = len (bytelist)
+        while (len(bytelist)>wsize):
+            print (str(len(bytelist)/wsize) + " / " + str(lenn))
+            rl = []
+            for i in range(wsize):
+                rl= [bytelist.pop(0)] + rl
+            result=result +rl
+        result = result + bytelist
+        return result
+
+
+
+def printHelp():
+    print("Usage of EndianSwap.py: EndianSwap.py filepath Wordsize")
+
+def main():
+    arguments=sys.argv[1:]
+    argc= len(arguments)
+    result=()
+    if(len(arguments)!=2):
+        printHelp()
+        exit()
+    else:
+        result = EndSwap(arguments[0],int(arguments[1])).result
+        open(arguments[0]+".rev", "wb").write(bytearray(result))
+        exit()
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
